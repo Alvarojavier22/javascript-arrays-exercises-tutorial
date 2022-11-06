013d6734f34ffc28638a7cd70190a663533c17b4
Alvaro Javier <alvarojavierchagas@hotmail.com>
2022-11-06 23:03:34 +0000

    ejercicio 16 pronto, sigue faltando el 10

--- exercises/12-Delete-element/app.js
+++ exercises/12-Delete-element/app.js
@@ -2,6 +2,14 @@ let people = ['juan','ana','michelle','daniella','stefany','lucy','barak', 'emil
 
 //your code below
 
+
+const deletePerson = (people) => {
+    let newArray = []
+    //your code here
+    firstArray.forEach(item => {
+        newArray.filter()
+    })}
+
 console.log(deletePerson('daniella'));
 console.log(deletePerson('juan'));
 console.log(deletePerson('emilio'));
--- exercises/15-Max-integer-from-array/app.js
+++ exercises/15-Max-integer-from-array/app.js
@@ -1,1 +1,13 @@
 let myArray = [43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34];
+
+let mayor = 0;
+
+for ( let numero of myArray ) {
+
+  
+  if (mayor < numero)
+    mayor = numero;
+}
+
+
+console.log(mayor);
