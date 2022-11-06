7cb2228071cc58a0bfd644c8035b5f8508046b30
Alvaro Javier <alvarojavierchagas@hotmail.com>
2022-11-06 20:36:28 +0000

    Ejercicio 11 terminado

--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -1,3 +1,11 @@
 let mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 
 //your code here
+
+let mixType = []
+
+for (let i = 0; i < mix.length; i++){
+    let type = mix [i];
+mixType.push(typeof type)
+}
+console.log(mixType)
--- exercises/11-Do-while/app.js
+++ exercises/11-Do-while/app.js
@@ -1,7 +1,15 @@
-let i = 20;
+let i = 21;
 
 do {
     // Magic goes here;
     
+    i <= 20
     i--;
+    if(i == 0){
+        console.log("LIFTOFF")
+    }else if(i % 5 === 0){
+        console.log(i + '!')
+    }else{
+        console.log(i)
+    }
 } while (i > 0);
