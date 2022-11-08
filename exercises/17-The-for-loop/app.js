22b8c39173cd62a3c7744afcd5517d63ee8160c2
Alvaro Javier <alvarojavierchagas@hotmail.com>
2022-11-08 17:50:34 +0000

    20.2 pronto faltan 12 y 18

--- exercises/17-The-for-loop/app.js
+++ exercises/17-The-for-loop/app.js
@@ -1,9 +1,11 @@
 let myArray = [2323,4344,2325,324413,21234,24531,2123,42234,544,456,345,42,5445,23,5656,423];
 
+let total = 0;
+
 for (let number of myArray){
-	let total = 0;
-	total += myArray[number]
-	number = total / myArray.length
-	console.log(number);
+	total += number
 	
 }
+let promedio = total / myArray.length
+	
+	console.log(promedio);
--- exercises/20.2-More-mapping/app.js
+++ exercises/20.2-More-mapping/app.js
@@ -3,7 +3,8 @@
 let myNumbers = [23,234,345,4356234,243,43,56,2];
 
 // your code here
-var myFunction = myNumbers.map(function (newArray){
+let newArray = [];
+ const myFunction = myNumbers.map(function (newArray){
     return newArray * 3
 })
 
